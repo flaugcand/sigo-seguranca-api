2329c0d8c485fb683fe8ffc3fd529d04301949ef
flavio <>
2021-04-06 22:05:20 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/sigo/indtexbr/SigoSegurancaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.sigo.indtexbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigoSegurancaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigoSegurancaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sigo/indtexbr/SigoSegurancaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sigo.indtexbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigoSegurancaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
